feade7a11156041d42f9063b715c323ef46934c7
Vittoriax1 <99565350+Vittoriax1@users.noreply.github.com>
2022-06-16 18:16:42 -0400

    Add files via upload

---
+++ colorful.sh
@@ -0,0 +1,25 @@
+#!/bin/zsh
+# My first shell script
+clear
+echo -e "\033[1m Hello World"
+# Bold Effect
+echo -e "\033[5m Hello World"
+# Blink Effect
+echo -e "\033[0m Hello World"
+# Back to Normal
+echo -e "\033[31m Hello World"
+# Red
+echo -e "\033[32m Hello World"
+# Green
+echo -e "\033[33m Hello World"
+# See remaining on the screen
+echo -e "\033[34m Hello World"
+echo -e "\033[35m Hello World"
+echo -e -n "\033[0m"
+# Back to Normal
+echo -e "\033[41m Hello World"
+echo -e "\033[42m Hello World"
+echo -e "\033[43m Hello World"
+echo -e "\033[44m Hello World"
+echo -e "\033[45m Hello World"
+echo -e "\033[0m Hello World"
